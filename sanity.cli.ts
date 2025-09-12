0af4777e8510b68af972c7bfede2f71042bfc547
insid30ut <mycelialfunguy@gmail.com>
2025-09-11 22:43:19 -0500

    fix: correct formatting by moving studioHost declaration in CLI configuration

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -3,9 +3,10 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'y0l7x6o2',
-    dataset: 'production',
-    studioHost: 'mycelial-underground'
+    dataset: 'production'
+    
   },
+  studioHost: 'mycelial-underground',
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
